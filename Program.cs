ae987e93e59e7e2195c2f87b576b849405e92229
Luis Davi Vieira SIlva <85641308+LuisDaviOFC@users.noreply.github.com>
2022-09-15 10:06:27 -0300

    Conhecendo as Estruturas de Repetição em C#

--- Program.cs
+++ Program.cs
@@ -1,16 +1,79 @@
 ﻿using SITE_DIO_BOOTCAMP_Pottencial_.NET_Developer.Models;
 
-        Calculadora calc = new Calculadora();
-        calc.Somar (10, 30);
-        calc.Subtrair (10, 50);
-        calc.Multiplicar (15, 45);
-        calc.Dividir (2, 2);
-        calc.Potencia (2, 10);
-        calc.Seno(30);
-        calc.Conseno(30);
-        calc.Tangente(30);
-        calc.RaizQuadrada(9);
-        
+string opcao;
+bool exibirMenu = true;
+
+while(exibirMenu){
+  Console.Clear();
+  Console.WriteLine("Digite a sua opção: ");
+  Console.WriteLine("1 - Cadastrar clientes");
+  Console.WriteLine("2 - Buscar clientes");
+  Console.WriteLine("3 - Apagar clientes");
+  Console.WriteLine("4 - Encerrar");
+
+  opcao = Console.ReadLine();
+
+  switch(opcao){
+    case "1":
+    Console.WriteLine("Cadastro de Cliente");
+    break;
+    case "2":
+    Console.WriteLine("Busca de Cliente");
+    break;
+    case "3":
+    Console.WriteLine("Apagar Cliente");
+    break;
+    case "4":
+    Console.WriteLine("Encerrar");
+    exibirMenu = false;
+    //Environment.Exit(0);
+    break;
+    default:
+    Console.WriteLine("Opcao inválida");
+    break;
+  }
+}
+
+//int soma = 0, numero = 0;
+
+//do{
+//  Console.WriteLine("Digite um número (0 para parar)");
+//  numero = Convert.ToInt32(Console.ReadLine());
+
+//  soma += numero;
+   
+//}while(numero != 0);
+
+//Console.WriteLine($"Total da soma dos números digitados é: {soma}");
+
+//int numero = 5;
+//int contador = 0;
+
+//while (contador <= 10){
+//  Console.WriteLine($"{contador + 1} x Execução: {numero} x {contador} = {numero * contador}");
+//  contador++;
+//  if (contador == 6){
+//    break;
+//  }
+//}
+
+//int numero = 10;
+
+//for(int contador = 0; contador <= 10; contador++){
+//  Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+//}
+
+//       Calculadora calc = new Calculadora();
+//       calc.Somar (10, 30);
+//        calc.Subtrair (10, 50);
+//        calc.Multiplicar (15, 45);
+//        calc.Dividir (2, 2);
+//        calc.Potencia (2, 10);
+//        calc.Seno(30);
+//        calc.Conseno(30);
+//        calc.Tangente(30);
+//        calc.RaizQuadrada(9);
+
 //bool choveu = true;
 //bool estatarde = false;
 
