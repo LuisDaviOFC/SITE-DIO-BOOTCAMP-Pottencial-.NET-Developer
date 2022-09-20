1ff039a7b1416358827296792ae62c86146d8947
Luis Davi Vieira SIlva <85641308+LuisDaviOFC@users.noreply.github.com>
2022-09-20 16:28:23 -0300

    desafioscodigobasico

---
+++ DesafiosCodigoBasico/desafio1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class DIO {
+        
+static void Main(string[] args){
+
+        int n = int.Parse(Console.ReadLine());
+        int p = 1, s = 0;
+        while (n > 0){
+             int l = n % 10;
+// TODO: Crie as outras condições necessárias para a resolução do desafio:
+             p = p * l;
+             s = s + l;
+             n = (n - l) / 10;
+           }
+
+           Console.WriteLine(p-s);
+        }
+    }
