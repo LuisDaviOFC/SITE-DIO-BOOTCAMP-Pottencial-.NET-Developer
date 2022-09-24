d6c6d549db05625463cc36e7545f1b231b77ba5e
Luis Davi Vieira SIlva <85641308+LuisDaviOFC@users.noreply.github.com>
2022-09-24 10:40:18 -0300

    atualização

---
+++ Atividade1/Program.cs
@@ -0,0 +1,12 @@
+﻿using Atividade1.models;
+
+Pessoa p1 = new Pessoa(nome: "Luis Davi",sobrenome: "Vieira Silva");
+Pessoa p2 = new Pessoa(nome: "Carlos",sobrenome: "Murilo");
+
+Curso CursoDeIngles = new Curso();
+CursoDeIngles.Nome = "Ingles";
+CursoDeIngles.Alunos = new List<Pessoa>();
+
+CursoDeIngles.AdicionarAluno(p1);
+CursoDeIngles.AdicionarAluno(p2);
+CursoDeIngles.ListarAlunos();
---
+++ Atividade1/models/Pessoa.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Atividade1.models
+{
+    public class Pessoa
+    {
+        public Pessoa(){
+
+        }
+        public Pessoa(string nome, string sobrenome){
+            Nome = nome;
+            Sobrenome = sobrenome;
+        }
+        private string _nome;
+        private string _sobrenome;
+        private int _idade;
+        public string Nome { 
+            get =>_nome;
+            set{
+                if(value == ""){
+                    throw new ArgumentException("O nome não pode ser vazio");
+                }
+                _nome = value;
+            } 
+        }
+
+        public string Sobrenome { 
+            get =>_sobrenome; 
+            set{
+                if(value == ""){
+                    throw new ArgumentException("O nome não pode ser vazio");
+                }
+                _sobrenome = value;
+            }
+        } 
+
+        public string NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();
+
+        public int Idade { 
+            get => _idade;
+            set{
+                if(value < 0){
+                    throw new ArgumentException("A idade não pode ser negativa");
+                }
+
+                _idade = value;
+            } 
+        }
+
+        public void apresentar(){
+            Console.WriteLine($"Olá, meu nome é {NomeCompleto} e tenho {Idade} anos.");
+    }
+}
+}
